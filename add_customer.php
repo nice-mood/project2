6d4bc9009c4463aa8197ad9fcf95a6e8f7a43139
Unknown <heli.isohatala@gmail.com>
2017-03-17 11:41:17 +0200

    enempi shittii

--- add_customer.php
+++ add_customer.php
@@ -2,7 +2,7 @@
 
 <h2>Add Customers</h2>
 
-<form class="" action="add_customer.php" method="post">
+<form class="" action="customers.php" method="post">
     <label for="fn">Firstname</label> <br />
     <input type="text" name="fn" id="fn" value="" required><br />
 
@@ -16,21 +16,6 @@
 
 </form>
 
-<?php 
-include "connect.php" ;
-$btn=$_POST['btnAdd'] ;
-    if(isset($btn))
-    {
-        $add=$db->prepare("INSERT INTO customers (firstname,lastname,streetadress) VALUES (:fn, :ln, :ad)") ;
-        $add->bindParam(':fn', $fn);
-        $add->bindParam(':ln', $ln);
-        $add->bindParam(':ad', $ad);
 
-        $fn=$_POST['fn'] ;
-        $ln=$_POST['ln'] ;
-        $ad=$_POST['ad'] ;
-        $add->execute();
-    }
-?>
 
 <?php include "footer.php" ?>
